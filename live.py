dc732a8eccfeb02bce1a3e909aa557c3488fe671
Safa <safasafari.ss@gmail.com>
2021-09-13 01:17:37 +0000

    Update live.py

--- live.py
+++ live.py
@@ -1,13 +1,16 @@
 import asyncio
 import pytgcalls
 import pyrogram
+import streamlink
 import sys
 
 
 async def main(client):
     group_call = pytgcalls.GroupCallFactory(client).get_group_call()
     await group_call.join(sys.argv[3])
-    stream = sys.argv[2]
+    stream = sys.argv[4]
+    if not sys.argv[4].__contains__('m3u8'):
+        stream = streamlink.streams(sys.argv[4])['720p'].url
     await group_call.start_video(stream)
 
     await pyrogram.idle()
